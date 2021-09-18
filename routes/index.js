24855a33f7f3a2ecb2ffaee89c8c5c756264c85f
luciussulla <maciek.kowalczyk@yandex.com>
2021-09-18 19:05:37 +0200

    Fire request to API on pageload and return movie posters

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
-const apiRoute = 'http://www.omdbapi.com/?i=tt3896198&apikey=3d759e78'
-
+// const apiRoute = 'http://www.omdbapi.com/?i=tt3896198&apikey=3d759e78'
+const apiRoute = 'http://www.omdbapi.com/?apikey=3d759e78&s=Batman'
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request.get(apiRoute, (err, response, data)=> {
+    const parsedData = JSON.parse(data)
+
+    res.render('index', {
+      parsedData: parsedData.Search
+    })
+  })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
