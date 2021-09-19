8b2a39ec10ca3dfc9fe7ee481b1a42ea886c75f3
luciussulla <maciek.kowalczyk@yandex.com>
2021-09-19 17:54:57 +0200

    Create route for individual movie and get the data from API for that movie and show it

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 // const apiRoute = 'http://www.omdbapi.com/?i=tt3896198&apikey=3d759e78'
+const rootUrl = 'http://www.omdbapi.com/?apikey=3d759e78&'
 const apiRoute = 'http://www.omdbapi.com/?apikey=3d759e78&s=Batman'
 const request = require('request');
 
 /* GET home page. */
+router.use((req,res,next)=> {
+  res.locals.rootUrl = rootUrl
+  next()
+})
+
 router.get('/', function(req, res, next) {
   request.get(apiRoute, (err, response, data)=> {
     const parsedData = JSON.parse(data)
@@ -12,8 +18,19 @@ router.get('/', function(req, res, next) {
     res.render('index', {
       parsedData: parsedData.Search
     })
+
   })
-  // res.render('index', { title: 'Express' });
 });
+
+router.get('/movie/:movieId', (req,res,next)=> {
+    const movieId = req.params.movieId
+    request.get(`${rootUrl}i=${movieId}`, (err, response, data)=> {
+      const movieData = JSON.parse(data)
+      res.render('movie', {
+        movieData
+      })
+    })
+})
+
 
 module.exports = router;
